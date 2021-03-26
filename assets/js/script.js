94db15fa94cfd03fc5547bc18605dc19e98a37e9
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-03-26 14:22:34 -0500

    linked js, added the current date with moment js, and created initial todo table

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+// Displays current date
+var currentDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
+
+function generateHours() {
+  var colDiv = $;
+}
